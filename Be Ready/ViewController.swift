3ff9e098400c17f05a47b04f06b9a5f8bd619309
Dasha Shevchenko <68771978+codrelia@users.noreply.github.com>
2021-10-28 16:49:28 +0300

    First commit

---
+++ Be Ready/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Be Ready
+//
+//  Created by Дарья Шевченко on 28.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
